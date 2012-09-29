096e4268366904a3a99b1201cfa087cb11c803a0
ISHIDA Akio <ishida@coderium.com>
2012-09-29 23:37:59 +0900

    キャシュされたオブジェクトのテスト

--- src/mrno/TimedCache.php
+++ src/mrno/TimedCache.php
@@ -4,12 +4,20 @@ namespace mrno;
 class TimedCache
 {
     private $loader;
+    private $cachedValue;
 
     function __construct(ObjectLoader $loader) {
+        $this->cachedValue = [];
         $this->loader = $loader;
     }
 
     function lookup($key) {
-        return $this->loader->load($key);
+        if (!array_key_exists($key, $this->cachedValue)) {
+            $value = $this->loader->load($key);
+            $this->cachedValue[$key] = $value;
+        } else {
+            $value = $this->cachedValue[$key];
+        }
+        return $value;
     }
 }
--- tests/mockery/TimedCacheTest.php
+++ tests/mockery/TimedCacheTest.php
@@ -29,4 +29,20 @@ class TimedCacheTest extends \PHPUnit_Framework_TestCase
         $this->assertThat($cache->lookup("KEY"), $this->equalTo("VALUE"));
         $this->assertThat($cache->lookup("KEY2"), $this->equalTo("VALUE2"));
     }
+
+
+    /** @test */
+    function キャッシュされたオブジェクトのロード() {
+        $mockLoader = m::mock('mrno\\ObjectLoader');
+
+        $mockLoader->shouldReceive('load')
+            ->with('KEY')
+            ->once()
+            ->andReturn('VALUE');
+
+        $cache = new TimedCache($mockLoader);
+        $this->assertThat($cache->lookup("KEY"), $this->equalTo("VALUE"));
+        $this->assertThat($cache->lookup("KEY"), $this->equalTo("VALUE"));
+
+    }
 }
