4f90ce5159c348b1572e677beffce8e5afae0b79
ISHIDA Akio <ishida@coderium.com>
2012-10-08 19:52:12 +0900

    タイムアウト後にオブジェクトがリロードされるテストの追加

--- tests/mockery/TimedCacheTest.php
+++ tests/mockery/TimedCacheTest.php
@@ -18,7 +18,6 @@ class TimedCacheTest extends \PHPUnit_Framework_TestCase
 
         $mockClock->shouldReceive('getCurrentTime')
             ->withNoArgs()
-            ->times(2)
             ->andReturn(strtotime('2012-01-01 00:00:00'));
 
         $mockLoader->shouldReceive('load')
@@ -69,5 +68,40 @@ class TimedCacheTest extends \PHPUnit_Framework_TestCase
         $cache = new TimedCache($mockLoader, $mockClock, $mockReloadPolicy);
         $this->assertThat($cache->lookup("KEY"), $this->equalTo("VALUE"));
         $this->assertThat($cache->lookup("KEY"), $this->equalTo("VALUE"));
+    }
+
+
+    /** @test */
+    function タイムアウト後にオブジェクトがロードされる()
+    {
+        // 一度目のlookupでloadしてキャッシュに格納し、
+        // 二度目のlookupでキャッシュから引き出すが、
+        // タイムアウトしているため再びloadする
+        $loadTime   = strtotime('2012-01-01 00:00:00');
+        $fetchTime  = strtotime('2012-01-01 01:00:00');
+        $reloadTime = strtotime('2012-01-01 02:00:00');
+
+        $mockClock = m::mock('mrno\\Clock');
+        $mockLoader = m::mock('mrno\\ObjectLoader');
+        $mockReloadPolicy = m::mock('mrno\\ReloadPolicy');
+
+        $mockClock->shouldReceive('getCurrentTime')
+            ->withNoArgs()
+            ->times(3)
+            ->andReturn($loadTime, $fetchTime, $reloadTime);
+
+        $mockLoader->shouldReceive('load')
+            ->with('KEY')
+            ->times(2)
+            ->andReturn('VALUE', 'NEW_VALUE');
+
+        $mockReloadPolicy->shouldReceive('shouldReload')
+            ->with($loadTime, $fetchTime)
+            ->atLeast()->once()
+            ->andReturn(true);
+
+        $cache = new TimedCache($mockLoader, $mockClock, $mockReloadPolicy);
+        $this->assertThat($cache->lookup("KEY"), $this->equalTo("VALUE"));
+        $this->assertThat($cache->lookup("KEY"), $this->equalTo("NEW_VALUE"));
     }
 }
